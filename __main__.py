8d55e5db5a83a606d49f23868e3f074c93776265
Caio <cjlcarvalho@live.com>
2017-10-27 21:18:26 -0300

    Mudando para python3

--- __main__.py
+++ __main__.py
@@ -2,10 +2,10 @@ from Scraper import Scraper
 import os
 
 def main():
-    folder = raw_input("Select your download folder: ")
+    folder = input("Select your download folder: ")
     if not os.path.exists(folder):
         os.makedirs(folder)
-    page = raw_input("Select your file page: ")
+    page = input("Select your file page: ")
     scraper = Scraper(page, folder)
     scraper.getFiles()
     
